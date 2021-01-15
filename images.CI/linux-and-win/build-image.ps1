e73de5b3b276cb442a2b80961708a90f6a3c7fa7
Dan Piessens <dan.piessens@tcreveal.com>
2021-01-15 13:19:22 -0600

    enable public IP

--- images.CI/linux-and-win/build-image.ps1
+++ images.CI/linux-and-win/build-image.ps1
@@ -55,6 +55,7 @@ packer build    -var "capture_name_prefix=$ResourcesNamePrefix" `
                 -var "virtual_network_resource_group_name=$VirtualNetworkRG" `
                 -var "virtual_network_subnet_name=$VirtualNetworkSubnet" `
                 -var "run_validation_diskspace=$env:RUN_VALIDATION_FLAG" `
+                -var "private_virtual_network_with_public_ip=true" `
                 $TemplatePath `
         | Where-Object {
             #Filter sensitive data from Packer logs
